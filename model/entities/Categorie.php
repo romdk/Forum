dc182a26fc8cc5ee59910b072cd801646aefd7a0
romdk <drack.roman67@gmail.com>
2022-11-28 14:17:34 +0100

    entitée categorie

---
+++ model/entities/Categorie.php
@@ -0,0 +1,54 @@
+<?php
+    namespace Model\Entities;
+
+    use App\Entity;
+
+    final class Categorie extends Entity{
+
+        private $id;
+        private $titre;
+
+        public function __construct($data){         
+            $this->hydrate($data);        
+        }
+ 
+        /**
+         * Get the value of id
+         */ 
+        public function getId()
+        {
+                return $this->id;
+        }
+
+        /**
+         * Set the value of id
+         *
+         * @return  self
+         */ 
+        public function setId($id)
+        {
+                $this->id = $id;
+
+                return $this;
+        }
+
+        /**
+         * Get the value of nomCategorie
+         */ 
+        public function getNomCategorie()
+        {
+                return $this->nomCategorie;
+        }
+
+        /**
+         * Set the value of nomCategorie
+         *
+         * @return  self
+         */ 
+        public function setNomCategorie($nomCategorie)
+        {
+                $this->nomCategorie = $nomCategorie;
+
+                return $this;
+        }
+    }
