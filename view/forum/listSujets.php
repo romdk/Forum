71111759aeebbc0e9cf0a78e5db01063bb838ad8
romdk <drack.roman67@gmail.com>
2022-11-28 14:52:25 +0100

    list sujets

---
+++ view/forum/listSujets.php
@@ -0,0 +1,18 @@
+<?php
+
+$sujets = $result["data"]['sujets'];
+    
+?>
+
+<h1>liste sujets</h1>
+
+<?php
+foreach($sujets as $sujet ){
+
+    ?>
+    <p><?=$sujet->getTitre()?></p>
+    <?php
+}
+
+
+  
