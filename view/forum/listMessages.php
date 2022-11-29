92f73d55cbd5ac92b4bf3dd232f4abb8d7e47421
romdk <drack.roman67@gmail.com>
2022-11-29 21:30:17 +0100

    list messages

--- view/forum/listMessages.php
+++ view/forum/listMessages.php
@@ -4,22 +4,34 @@ $messages = $result["data"]['messages'];
     
 ?>
 
-<div>
+<div class="infoSujet">
     <p>Titre du sujet</p>
     <p>auteur du sujet</p>
     <p>date de creation du sujet</p>
 </div>
 
-<?php
-foreach($messages as $message){
-
-    ?>
-    <div class="message2">
-        <p><?=$message->getVisiteur()->getPseudonyme()?></p>
-        <p><?=$message->getVisiteur()->getRole()?></p>
-        <p><?=$message->getDateCreation()?></p>
-        <p><?=$message->getTexte()?></p>
-        <button><i class="fa-regular fa-comment"></i><input class="hidden" type="text"></button>
-    </div>
+<div class="listeMessages">
     <?php
-}
+    foreach($messages as $message){
+        ?>            
+            <div class="message2">
+                <p class="pseudo"><?=$message->getVisiteur()->getPseudonyme()?></p>
+                <p class="role"><?=$message->getVisiteur()->getRole()?></p>
+                <p class="date"><?=$message->getDateCreation()?></p>
+                <p class="texte"><?=$message->getTexte()?></p>
+                <div class="interactions">
+                    <div class="repondre">
+                        <button><i class="fa-regular fa-comment"></i> Répondre<input class="hidden" type="text"></button>
+                    </div>
+                    <div class="vote">
+                        <button><i class="fa-solid fa-up-long"></i></button>
+                        <p><?=$message->getNbVote()?></p>
+                        <button><i class="fa-solid fa-down-long"></i></button>
+                    </div>
+                </div>
+            </div>            
+        <?php
+    }
+?>
+
+
