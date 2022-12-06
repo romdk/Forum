2ab239c9bb41580c38995ccb3163f2b4ca44d2aa
romdk <drack.roman67@gmail.com>
2022-12-06 16:24:59 +0100

    aff dernier message

--- view/security/listeVisiteurs.php
+++ view/security/listeVisiteurs.php
@@ -1,17 +1,20 @@
 <?php
+    use Model\Managers\MessageManager;
     $visiteurs = $result["data"]['visiteurs'];    
-?>
+    ?>
 <div class="listeVisiteurs">
     <ul>
         <?php
         foreach($visiteurs as $visiteur){
-
+            $messageManager = new MessageManager();
+            $id = $visiteur->getId();
             ?>
-            <li>
+            <li class='infosVisiteur'>
                 <p>Pseudonyme: <?= $visiteur->getPseudonyme()?></p>
                 <p>Inscrit le <?= $visiteur->getDateInscription()?></p>
                 <p>Role: <?= $visiteur->getRole()?></p>
                 <p>Mail: <?= $visiteur->getMail()?></p>
+                <p>dernier message: <?= $messageManager->findLastMessageFromVisiteur($id)?></p>
             </li>
             <?php
         }
