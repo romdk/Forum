c094d51d4bf88c763c735cb509924eea8f34ac22
romdk <drack.roman67@gmail.com>
2022-12-06 09:53:45 +0100

    style

--- view/home.php
+++ view/home.php
@@ -1,5 +1,7 @@
-<h1>BIENVENUE SUR LE FORUM</h1>
+<div id='home'>
+    <h1>BIENVENUE SUR LE FORUM</h1>
 
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit ut nemo quia voluptas numquam, itaque ipsa soluta ratione eum temporibus aliquid, facere rerum in laborum debitis labore aliquam ullam cumque.</p>
+    <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.<p>
 
-<p><a href="index.php?ctrl=forum&action=listCategories">Entrer</a></p>
+    <p id='entrer'><a href="index.php?ctrl=forum&action=listCategories">Entrer</a></p>
+</div>
