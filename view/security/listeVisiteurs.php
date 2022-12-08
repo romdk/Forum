3b5331911eb1735f2cb05c138bab9c60d0261e40
romdk <drack.roman67@gmail.com>
2022-12-08 14:03:18 +0100

    redirection profil

--- view/security/listeVisiteurs.php
+++ view/security/listeVisiteurs.php
@@ -10,7 +10,7 @@
             $id = $visiteur->getId();
             ?>
             <li class='infosVisiteur'>
-                <span class='intitule'>Pseudonyme: </span><span class='contenu'> <?= $visiteur->getPseudonyme()?></span>
+                <span class='intitule'>Pseudonyme: </span><a href="index.php?ctrl=forum&action=profilVisiteur&id=<?=$id ?>" class='contenu'> <?= $visiteur->getPseudonyme()?></a>
                 <span class='intitule'>Inscrit le </span><span class='contenu'> <?= $visiteur->getDateInscription()?></span>
                 <span class='intitule'>Role: </span><span class='contenu'> <?= $visiteur->getRole()?></span>
                 <span class='intitule'>Mail: </span><span class='contenu'> <?= $visiteur->getMail()?></span>
