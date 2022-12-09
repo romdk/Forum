6c613002d2c3029dc1aacbffdb74d095a9cbbb69
romdk <drack.roman67@gmail.com>
2022-12-09 10:49:22 +0100

    changement affichage infos visiteur

--- view/forum/profilVisiteur.php
+++ view/forum/profilVisiteur.php
@@ -9,14 +9,14 @@ $visiteur = $visiteurManager->findOneById($id);
             ?>
             <div class="profil">
                 <div class='infosVisiteur'>
-                    <span class='intitule'>Pseudonyme: </span><span class='contenu'> <?= $visiteur->getPseudonyme()?></span>
-                    <span class='intitule'>Inscrit le </span><span class='contenu'> <?= $visiteur->getDateInscription()?></span>
-                    <span class='intitule'>Role: </span><span class='contenu'> <?= $visiteur->getRole()?></span>
-                    <span class='intitule'>Mail: </span><span class='contenu'> <?= $visiteur->getMail()?></span>
-                    <span class='intitule'>dernier message: </span>
+                    <div><span class='intitule'>Pseudonyme: </span><span class='contenu'> <?= $visiteur->getPseudonyme()?></span></div>
+                    <div><span class='intitule'>Inscrit le </span><span class='contenu'> <?= $visiteur->getDateInscription()?></span></div>
+                    <div><span class='intitule'>Role: </span><span class='contenu'> <?= $visiteur->getRole()?></span></div>
+                    <div><span class='intitule'>Mail: </span><span class='contenu'> <?= $visiteur->getMail()?></span></div>
+                    <div><span class='intitule'>dernier message: </span></div>
                     <?php
                     if($messageManager->findLastMessageFromVisiteur($id) !== false){ ?>
-                        <span class='contenu'> <?= $messageManager->findLastMessageFromVisiteur($id)?></span>
+                       <div><span class='contenu'> <?= $messageManager->findLastMessageFromVisiteur($id)?></span></div>
                     <?php
                     }else { ?>
                         <span>Aucun message</span>
--- view/security/listeVisiteurs.php
+++ view/security/listeVisiteurs.php
@@ -10,14 +10,14 @@
             $id = $visiteur->getId();
             ?>
             <li class='infosVisiteur'>
-                <span class='intitule'>Pseudonyme: </span><a href="index.php?ctrl=forum&action=profilVisiteur&id=<?=$id ?>" class='contenu'> <?= $visiteur->getPseudonyme()?></a>
-                <span class='intitule'>Inscrit le </span><span class='contenu'> <?= $visiteur->getDateInscription()?></span>
-                <span class='intitule'>Role: </span><span class='contenu'> <?= $visiteur->getRole()?></span>
-                <span class='intitule'>Mail: </span><span class='contenu'> <?= $visiteur->getMail()?></span>
-                <span class='intitule'>dernier message: </span>
+                <div><span class='intitule'>Pseudonyme: </span><a href="index.php?ctrl=forum&action=profilVisiteur&id=<?=$id ?>" class='contenu'> <?= $visiteur->getPseudonyme()?></a></div>
+                <div><span class='intitule'>Inscrit le </span><span class='contenu'> <?= $visiteur->getDateInscription()?></span></div>
+                <div><span class='intitule'>Role: </span><span class='contenu'> <?= $visiteur->getRole()?></span></div>
+                <div><span class='intitule'>Mail: </span><span class='contenu'> <?= $visiteur->getMail()?></span></div>
+                <div><span class='intitule'>dernier message: </span></div>
                 <?php
                 if($messageManager->findLastMessageFromVisiteur($id) !== false){ ?>
-                    <span class='contenu'> <?= $messageManager->findLastMessageFromVisiteur($id)?></span>
+                    <div><span class='contenu'> <?= $messageManager->findLastMessageFromVisiteur($id)?></span></div>
                 <?php
                 }else { ?>
                 <span>Aucun message</span>
