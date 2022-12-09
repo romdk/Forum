0d55752b193fe5551a2ee226f52fd347ca9d1d67
romdk <drack.roman67@gmail.com>
2022-12-09 15:33:16 +0100

    fonction 5 dernieres activités

--- model/managers/VisiteurManager.php
+++ model/managers/VisiteurManager.php
@@ -1,81 +1,94 @@
 <?php
-    namespace Model\Managers;
-    
-    use App\Manager;
-    use App\DAO;
+  namespace Model\Managers;
+  
+  use App\Manager;
+  use App\DAO;
 
-    class VisiteurManager extends Manager{
+  class VisiteurManager extends Manager{
 
-      protected $className = "Model\Entities\Visiteur";
-      protected $tableName = "visiteur";
+    protected $className = "Model\Entities\Visiteur";
+    protected $tableName = "visiteur";
 
 
-      public function __construct(){
-          parent::connect();
-      }
+    public function __construct(){
+        parent::connect();
+    }
 
-      public function findOneByPseudo($pseudonyme) {
-        $sql = "SELECT * FROM $this->tableName WHERE pseudonyme = :pseudonyme"; 
-            
-        return $this->getOneOrNullResult(
-          DAO::select($sql,['pseudonyme' => $pseudonyme], false),
-          $this->className
-        );          
-      }
-
-      public function findOneById($id) {
-        $sql = "SELECT * FROM $this->tableName WHERE id_visiteur = :id"; 
-            
-        return $this->getOneOrNullResult(
-          DAO::select($sql,['id' => $id], false),
-          $this->className
-        );          
-      }
-
-      public function findOneByMail($mail) {
-          $sql = "SELECT * FROM $this->tableName WHERE mail = :mail";    
-          return $this->getOneOrNullResult(
-             DAO::select($sql,['mail' => $mail], false),
-             $this->className
-          );
-
+    public function findOneByPseudo($pseudonyme) {
+      $sql = "SELECT * FROM $this->tableName WHERE pseudonyme = :pseudonyme"; 
           
-      }
+      return $this->getOneOrNullResult(
+        DAO::select($sql,['pseudonyme' => $pseudonyme], false),
+        $this->className
+      );          
+    }
 
-      public function getMotDePasseBdd($pseudonyme){
-        $sql = "SELECT motDePasse FROM $this->tableName WHERE pseudonyme = :pseudonyme";
-       
-        return $this->getOneOrNullResult(
-          DAO::select($sql,['pseudonyme' => $pseudonyme], false),
+    public function findOneById($id) {
+      $sql = "SELECT * FROM $this->tableName WHERE id_visiteur = :id"; 
+          
+      return $this->getOneOrNullResult(
+        DAO::select($sql,['id' => $id], false),
+        $this->className
+      );          
+    }
+
+    public function findOneByMail($mail) {
+      $sql = "SELECT * FROM $this->tableName WHERE mail = :mail";    
+      return $this->getOneOrNullResult(
+          DAO::select($sql,['mail' => $mail], false),
           $this->className
-        );
-      }
+      );
 
-      public function changerRole($id,$role){
-        $sql = "UPDATE $this->tableName
-                SET role = '$role'
-                WHERE id_".$this->tableName." = :id
-                ";
-        return DAO::update($sql, ['id' => $id]);
-      }
-
-      public function ban($id){
-        $sql = "UPDATE $this->tableName
-                SET statut = '1'
-                WHERE id_".$this->tableName." = :id
-                ";
-
-        return DAO::update($sql, ['id' => $id]); 
+        
     }
 
-      public function unban($id){
-        $sql = "UPDATE $this->tableName
-                SET statut = '0'
-                WHERE id_".$this->tableName." = :id
-                ";
-
-        return DAO::update($sql, ['id' => $id]); 
+    public function getMotDePasseBdd($pseudonyme){
+      $sql = "SELECT motDePasse FROM $this->tableName WHERE pseudonyme = :pseudonyme";
+      
+      return $this->getOneOrNullResult(
+        DAO::select($sql,['pseudonyme' => $pseudonyme], false),
+        $this->className
+      );
     }
 
-
+    public function changerRole($id,$role){
+      $sql = "UPDATE $this->tableName
+              SET role = '$role'
+              WHERE id_".$this->tableName." = :id
+              ";
+      return DAO::update($sql, ['id' => $id]);
     }
+
+    public function ban($id){
+      $sql = "UPDATE $this->tableName
+              SET statut = '1'
+              WHERE id_".$this->tableName." = :id
+              ";
+      return DAO::update($sql, ['id' => $id]); 
+    }
+
+    public function unban($id){
+      $sql = "UPDATE $this->tableName
+              SET statut = '0'
+              WHERE id_".$this->tableName." = :id
+              ";                
+      return DAO::update($sql, ['id' => $id]); 
+    }
+
+    public function find5LastActivityFromVisiteur($id){
+      $sql = "SELECT *
+      FROM message
+      WHERE visiteur_id = :id
+      UNION
+      SELECT *
+      FROM sujet
+      WHERE visiteur_id = :id
+      ORDER BY dateCreation DESC LIMIT 5";
+
+      return $this->getMultipleResults(
+        DAO::select($sql,['id' => $id]),
+        $this->className
+      );
+    }
+  }
+?>
