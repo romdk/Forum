a65fcb73a40b0b6618ab342ebb7e1d1fcda31506
romdk <drack.roman67@gmail.com>
2022-12-13 22:32:43 +0100

    formulaire modif profil

--- view/forum/profilVisiteur.php
+++ view/forum/profilVisiteur.php
@@ -6,6 +6,12 @@ $visiteurManager = new VisiteurManager();
 $messageManager = new MessageManager();
 $id=(isset($_GET["id"])) ? $_GET["id"] : null;
 $visiteur = $visiteurManager->findOneById($id);
+if(isset($_FILES['image'])){
+    var_dump('test');die;
+    $tmpName = $_FILES['image']['tmp_name'];
+    $name = $_FILES['image']['name'];
+    move_uploaded_file($tempName,'public/images'.$name);
+}
 // $activites = $visiteurManager->find5LastActivityFromVisiteur($id)
             ?>
             <div class="profil">
@@ -24,11 +30,11 @@ $visiteur = $visiteurManager->findOneById($id);
                     <?php
                     }
                     if(App\Session::isAdmin()){ ?>
-                        <form action="index.php?ctrl=security&action=attribuerRole&id=<?=$id ?>" method="post">
+                        <form class="formRole" action="index.php?ctrl=security&action=attribuerRole&id=<?=$id ?>" method="post">
                             <select name="role" id="selectRole">
                                 <option value="User">User</option>
                                 <option value="Admin">Admin</option>
-                                <input class="btnValider" type="submit" name="validerRole" value="Valider">
+                                <input class="btnValider" type="submit" name="validerRole" value="Valider" enctype="multipart/form-data">
                             </select>
                         </form>
                         <?php
@@ -39,7 +45,21 @@ $visiteur = $visiteurManager->findOneById($id);
                             <a class="btnDebannir" href="index.php?ctrl=security&action=unbanVisiteur&id=<?=$id?>">Débannir</a>
                         <?php
                         }
-                    }
-                        ?>
+                    } ?>
                 </div>
+                    <?php 
+                    if(App\Session::getVisiteur()->getId() == $id){?>
+                    <div class="modifProfil">
+                        Modifier profil
+                        <form class="formModif" action="index.php?ctrl=security&action=modifierProfil&id=<?=$id?>" method='post'>
+                            <input class="champPseudo" type="text" name="pseudonyme" placeholder="Saisir un pseudonyme">
+                            <input class="champMail" type="email" name="mail" placeholder="Saisir une adresse email valide">
+                            <input class="champImage" type="file" name="image">
+                            <input class="btnValider" type="submit" name="validerChangement" value="Valider">
+                        </form>
+                    </div>
+
+                    <?php 
+                    }
+                        ?>  
             </div>
