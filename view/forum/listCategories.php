f77076ed35eebcae2bcb5a693fd320252ccc8099
romdk <drack.roman67@gmail.com>
2022-12-13 22:31:48 +0100

    ajouter/supprimer categorie

--- view/forum/listCategories.php
+++ view/forum/listCategories.php
@@ -4,15 +4,27 @@ $categories = $result["data"]['categories'];
     
 ?>
 <div class="listeCategories">
-    <?php
+<?php
+    if(App\Session::isAdmin()){ ?>
+        <form id="nouvelleCategorie" action='index.php?ctrl=forum&action=ajoutCategorie' method='post'>nouvelle catégorie
+            <input id="champCategorie" type="text" name="nomCategorie" placeholder="Saisir un nom de catégorie">
+            <input id="btnAjouter" type="submit" name="ajouterCategorie" value="Créer catégorie">
+        </form>
+    <?php 
+    } 
+
     foreach($categories as $categorie){
 
         ?>
-            <a href="index.php?ctrl=forum&action=listSujets&id=<?= $categorie->getId()?>">
-                <div class="categorie">
-                <p class="nomCategorie"><?=$categorie->getNomCategorie()?></p>
-                </div>
-            </a>
+            <div class="categorie">
+                <a href="index.php?ctrl=forum&action=listSujets&id=<?= $categorie->getId()?>">
+                <p class="nomCategorie"><?=$categorie->getNomCategorie()?></p></a>
+                <?php
+                    if(App\Session::isAdmin()) {
+                    ?>
+                        <a class="supprimer" href="index.php?ctrl=forum&action=supprimerCategorie&id=<?= $categorie->getId()?>"><i class="fa-solid fa-trash"></i>Supprimer<a>
+                    <?php } ?>
+            </div>
         <?php
     }
     ?>
