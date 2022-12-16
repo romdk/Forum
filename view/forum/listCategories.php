0e41716b2431e215d266593eb1ae0e788a260c11
romdk <drack.roman67@gmail.com>
2022-12-16 14:30:17 +0100

    rseponsive categories

--- view/forum/listCategories.php
+++ view/forum/listCategories.php
@@ -3,29 +3,31 @@
 $categories = $result["data"]['categories'];
     
 ?>
-<div class="listeCategories">
-<?php
-    if(App\Session::isAdmin()){ ?>
-        <form id="nouvelleCategorie" action='index.php?ctrl=forum&action=ajoutCategorie' method='post'>nouvelle catégorie
-            <input id="champCategorie" type="text" name="nomCategorie" placeholder="Saisir un nom de catégorie">
-            <input id="btnAjouter" type="submit" name="ajouterCategorie" value="Créer catégorie">
-        </form>
-    <?php 
-    } 
+<div class="contenuPage">
+    <div class="listeCategories">
+    <?php
+        if(App\Session::isAdmin()){ ?>
+            <form id="nouvelleCategorie" action='index.php?ctrl=forum&action=ajoutCategorie' method='post'><span>Nouvelle categorie</span>
+                <input id="champCategorie" type="text" name="nomCategorie" placeholder="Saisir un nom de catégorie">
+                <input id="btnAjouter" type="submit" name="ajouterCategorie" value="Créer catégorie">
+            </form>
+        <?php 
+        } 
 
-    foreach($categories as $categorie){ ?>
-        <div class="categorie">
-            <a href="index.php?ctrl=forum&action=listSujets&id=<?= $categorie->getId()?>">
-                <p class="nomCategorie"><?=$categorie->getNomCategorie()?></p>
-            </a>
-            <?php
-            if(App\Session::isAdmin()) {
-            ?>
-                <a class="supprimer" href="index.php?ctrl=forum&action=supprimerCategorie&id=<?= $categorie->getId()?>"><i class="fa-solid fa-trash"></i>Supprimer</a>
+        foreach($categories as $categorie){ ?>
+            <div class="categorie">
+                <a href="index.php?ctrl=forum&action=listSujets&id=<?= $categorie->getId()?>">
+                    <p class="nomCategorie"><?=$categorie->getNomCategorie()?></p>
+                </a>
                 <?php
-            } ?>
-        </div>
-        <?php
-    }
-    ?>
+                if(App\Session::isAdmin()) {
+                ?>
+                    <a class="supprimer" href="index.php?ctrl=forum&action=supprimerCategorie&id=<?= $categorie->getId()?>"><i class="fa-solid fa-trash"></i>Supprimer</a>
+                    <?php
+                } ?>
+            </div>
+            <?php
+        }
+        ?>
+    </div>
 </div>
